a110a41a92873c0a53bf59830efd776c9e66d501
ix3twastaken <harkoiiika38@gmail.com>
2025-07-07 21:19:20 +0300

    наладил отправку в нужный чат + убрал ссылку на картинку(text) при отправке

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import telebot
-import requests
-import json
+import telebot, requests, json
+from telebot import types
 import access_tokens as at
 
-API_TOKEN = at.bot_api_token
 
+API_TOKEN = at.bot_api_token #API бота телеграм
 bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler(content_types=['text'])
-def get_json(message):
-    res = requests.get(f'https://api.vk.com/method/wall.get?domain=nyaslav&count=2&access_token={at.access_token}&v=5.199')
-    data = json.loads(res.text)
-    bot.send_message(message.chat.id, f'Картиночка: {data["response"]["items"][1]["attachments"][0]["photo"]["sizes"][8]["url"]}')
+
+@bot.message_handler(commands=['send']) #Отправка картинки в чат по команде /send (тестовая версия)
+def send(message):
+
+    # Получение картинки из сообщества VK
+    image_url = requests.get(f'https://api.vk.com/method/wall.get?domain=nyaslav&count=2&access_token={at.access_token}&v=5.199')
+    data = json.loads(image_url.text)
+    image_url = data["response"]["items"][1]["attachments"][0]["photo"]["sizes"][8]["url"]
+
+    CHAT_ID = '-100' + at.my_chat_id #Конечный чат в которое отправляется сообщение (id = "-100" + "id чата")
+
+    bot.send_photo(chat_id=CHAT_ID, photo=image_url, message_thread_id=86031) #Отправка в чат
+
+
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
